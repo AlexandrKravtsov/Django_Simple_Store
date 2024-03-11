77b06dcd0cc5d76caa9afc3101300ba9406ae35a
kraft <krafstov@gmail.com>
2024-03-11 19:52:16 +0300

    users app login.html forms.py customisation form

--- users/forms.py
+++ users/forms.py
@@ -1,9 +1,22 @@
+from django import forms
+
 from django.contrib.auth.forms import AuthenticationForm
 
 from users.models import User
 
 
 class UserLoginForm(AuthenticationForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control py-4'
+
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Имя пользователя'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Введите пароль'}))
+
     class Meta:
         model = User
         fields = ('username', 'password')
+
+

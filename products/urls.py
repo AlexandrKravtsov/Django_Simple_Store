ce5415e5542f6d5d098311cf606f9f406b656d7e
kraft <krafstov@gmail.com>
2024-03-12 21:52:22 +0300

    users app login.html register.html revision

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from .views import products
+from .views import products, basket_add
 
 app_name = 'products'
 
 urlpatterns = [
     path('', products, name='products'),
+    path('basket/add/<int:product_id>/', basket_add, name='basket_add'),
 ]

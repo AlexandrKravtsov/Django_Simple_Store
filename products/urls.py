276168cde2c3c5ce3587adb910c4a65dd6aaba44
kraft <krafstov@gmail.com>
2024-03-12 22:35:28 +0300

    users app product app basket.html revision

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from .views import products, basket_add
+from .views import products, basket_add, basket_remove
 
 app_name = 'products'
 
 urlpatterns = [
     path('', products, name='products'),
     path('basket/add/<int:product_id>/', basket_add, name='basket_add'),
+    path('basket/remove/<int:basket_id>/', basket_remove, name='basket_remove'),
 ]

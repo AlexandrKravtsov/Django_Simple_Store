e5d5eed613e9bd0a463558a1c890ad769aab3fc5
kraft <krafstov@gmail.com>
2024-03-13 21:59:17 +0300

    product app next division into categories

--- products/admin.py
+++ products/admin.py
@@ -9,6 +9,15 @@ admin.site.register(Basket)
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'quantity')
-    fields = ('name', 'image', 'description', 'short_description', 'price', 'quantity', 'category')
+    list_display = ('name', 'price', 'quantity', 'category')
+    fields = ('name', 'image', 'description', 'short_description', ('price', 'quantity'), 'category')
     readonly_fields = ('image',)
+    ordering = ('name',)
+    search_fields = ('name',)
+
+
+class BasketAdminInline(admin.TabularInline):
+    model = Basket
+    fields = ('product', 'quantity', 'created_timestamp')
+    readonly_fields = ('product',  'created_timestamp',)
+    extra = 0
--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 
 from .models import User
+from products.admin import BasketAdminInline
 
-admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    inlines = (BasketAdminInline,)
+
+
+
